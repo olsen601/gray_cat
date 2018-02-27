30c4987139c4924eea16beee05956606a48a6f28
clara <clara@clara.com>
2018-02-26 20:38:12 -0600

    download cat pictures from Cat API and convert to grayscale

---
+++ cat.py
@@ -0,0 +1,40 @@
+import requests
+from PIL import Image
+import time
+import os
+
+cat_url = 'http://thecatapi.com/api/images/get?format=src&type=jpg'
+output_directory = 'out'
+
+def generate_filenames():
+    name = 'cat_picture_%d.jpg' % int(time.time())
+    bw_name = 'bw_' + name
+    return os.path.join(output_directory, name), os.path.join(output_directory, bw_name)  # Returning a tuple of values
+
+
+def download_cat_picture(filename):
+    response = requests.get(cat_url)
+    with open(filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=512):  # chunk_size is how many bytes are stored in memory at a time before writing to disk
+            file.write(chunk)
+
+
+def black_and_white_filter(filename, out_filename):
+    image = Image.open(filename)
+    bw_image = image.convert(mode='L')  # 'L' is greyscale
+    bw_image.save(out_filename)
+
+
+def main():
+    try:
+        filenames = generate_filenames()
+        download_cat_picture(filenames[0])
+        black_and_white_filter(*filenames)  # Tuple unpacking
+        print('Your cat picture is saved at %s\nThe black-and-white version is %s' % filenames) # Tuple as argument to string formatting
+
+    except Exception as e:
+        print('Error fetching image because', e)
+
+
+if __name__ == '__main__':
+    main()
