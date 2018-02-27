db8f05ff44916865b814178f0b063e22a6b0d2a7
clara <clara@clara.com>
2018-02-26 21:08:12 -0600

    add begins for command line arg parsing

--- cat.py
+++ cat.py
@@ -2,11 +2,12 @@ import requests
 from PIL import Image
 import time
 import os
+import begin
 
 cat_url = 'http://thecatapi.com/api/images/get?format=src&type=jpg'
-output_directory = 'out'
+# output_directory = 'out'
 
-def generate_filenames():
+def generate_filenames(output_directory):
     name = 'cat_picture_%d.jpg' % int(time.time())
     bw_name = 'bw_' + name
     return os.path.join(output_directory, name), os.path.join(output_directory, bw_name)  # Returning a tuple of values
@@ -25,16 +26,13 @@ def black_and_white_filter(filename, out_filename):
     bw_image.save(out_filename)
 
 
-def main():
+@begin.start
+def main(output_dir='out'):
     try:
-        filenames = generate_filenames()
+        filenames = generate_filenames(output_dir)
         download_cat_picture(filenames[0])
         black_and_white_filter(*filenames)  # Tuple unpacking
         print('Your cat picture is saved at %s\nThe black-and-white version is %s' % filenames) # Tuple as argument to string formatting
 
     except Exception as e:
         print('Error fetching image because', e)
-
-
-if __name__ == '__main__':
-    main()
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 Fetch a cat picture from The Cat API using requests, then convert it to grayscale using Pillow.
 
+Command line argument parsing with Begins.
+
 ## Takeaways
 
 * requirements.txt, virtual environments
@@ -13,3 +15,4 @@ Fetch a cat picture from The Cat API using requests, then convert it to grayscal
 * [The Cat API](http://thecatapi.com/)
 * [Requests](http://docs.python-requests.org/en/master/)
 * [Pillow](https://pillow.readthedocs.io/en/latest/)
+* [Begins](https://pypi.python.org/pypi/begins)
